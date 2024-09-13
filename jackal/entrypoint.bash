c507d3a99a875cbaa1f64be0325e53c828396d4e
jason <jasonah@seas.upenn.edu>
2024-09-13 12:11:54 -0400

    updating runtime configurability

--- jackal/entrypoint.bash
+++ jackal/entrypoint.bash
@@ -1,4 +1,5 @@
 #!/bin/bash
 source /opt/ros/noetic/setup.bash
 source ws/devel/setup.bash
+roscore &
 roslaunch jackal_launch jackal_hw.launch --wait rgb:=$RGB ir:=$IR event:=$EVENT gps:=$GPS ouster:=$OUSTER
